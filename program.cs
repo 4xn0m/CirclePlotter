8f6271e4f6be441ce205f47bf86a75f8d3054449
4xn0m <james.hobbs145@gmail.com>
2022-09-24 00:07:09 +0100

    CP gui setup

---
+++ program.cs
@@ -0,0 +1,11 @@
+// Hello World! program
+using System;
+namespace program
+{
+    class Hello {         
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Helldddddo World!");
+        }
+    }
+}
